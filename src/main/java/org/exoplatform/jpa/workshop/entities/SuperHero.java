c7db99ba8dca0acf9b71e0ee90429e2d9b55369f
Thomas Delhoménie <thomas.delhomenie@gmail.com>
2015-11-07 16:15:38 +0100

    Change columns name of SuperHero entity

--- src/main/java/org/exoplatform/jpa/workshop/entities/SuperHero.java
+++ src/main/java/org/exoplatform/jpa/workshop/entities/SuperHero.java
@@ -1,23 +1,26 @@
 package org.exoplatform.jpa.workshop.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  *
  */
 @Entity
+@Table(name = "SUPERHERO")
 public class SuperHero {
 
   @Id
   @GeneratedValue
+  @Column(name = "SUPERHERO_ID")
   private long id;
 
+  @Column(name = "NAME")
   private String name;
 
+  @Column(name = "CIVIL_NAME")
   private String civilName;
 
+  @Column(name = "PICTURE")
   private byte[] picture;
 
   public long getId() {
